a79d13319fd8c8ac405fd04f7e9f699b51c41834
carllb <carllb52@gmail.com>
2017-06-01 15:43:17 -0400

    init funciton + basic gameloop

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+//aliases for pixi container
+var stage = new PIXI.Container(),
+    renderer = PIXI.autoDetectRenderer(512, 512),
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    resources = PIXI.loader.resources;
+
 function init(){
-  
+
+  document.body.appendChild(renderer.view);
+
+  gameLoop();
+}
+
+function gameLoop(){
+
+  //Loop this function 60 times per second
+  requestAnimationFrame(gameLoop);
+
+  //Render the stage
+  renderer.render(stage);
 }
